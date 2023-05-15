5db901cb7ceb097406fb0bd96d121aa63ae71bf6
RanjitSinghChouhan <ranjeet.singh.chouhan94@gmail.com>
2023-05-15 11:18:28 +0530

    feat: added instructions in REPL

--- NodeJSConcepts/REPL/repl.js
+++ NodeJSConcepts/REPL/repl.js
@@ -1,9 +1,9 @@
-// REPL:
+// REPL(read-eval-print-loop): (to start simply write node and then enter on terminal)
 // 1: JS expression,
 // 2: use variables,
 // 3: multiple line code,
 // 4: use (_) to get the last result,
-// 5: we can use editor mode
+// 5: we can use editor mode (using .editor)
 
 
 // Firstly we need to write node and then enter to get the terminal similar to inspect console 
